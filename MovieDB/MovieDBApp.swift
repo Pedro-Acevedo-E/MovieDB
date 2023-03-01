7dc727b2085bfb9a72db441b36b5dbe89331e964
Pedro_Acevedo <acevedoensisopedrogabriel@gmail.com>
2023-02-28 17:30:44 -0800

    Initial commit Added .gitignore

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Pedro Acevedo on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
